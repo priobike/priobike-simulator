2d427c012e7cdf5d368ab9fcbd33f00b6dbd34be
Yenong Zhang <yenong.zhang@mailbox.tu-dresden.de>
2023-12-12 23:07:55 +0100

    Add function to place traffic lights based on app messages and test with mockdata

--- js/main.js
+++ js/main.js
@@ -52,9 +52,7 @@ window.onload = (event) => {
 
     const signalLayerIdPrefix = 'custom-threebox-signal-';
     signalColor = signalColors[currentSignalColorIndex];
-    //signalLayerId = `${signalLayerIdPrefix}${signalColor}`;
-    timestamp = new Date().getTime();
-    signalLayerId = `${signalLayerIdPrefix}${timestamp}`;
+    signalLayerId = createLayerID();
     //const signalLayerId = 'custom-threebox-signal';
     loadTrafficSignalModel(map,signalColor,signalLayerId);
 
@@ -65,13 +63,24 @@ window.onload = (event) => {
         currentSignalColorIndex = (currentSignalColorIndex + 1) % signalColors.length;
 
         const currentColor = signalColors[currentSignalColorIndex];
-        const newTimestamp = new Date().getTime();
-        //const newSignalLayerId = `${signalLayerIdPrefix}${currentSignalColorIndex}`;
-        const newSignalLayerId = `${signalLayerIdPrefix}${newTimestamp}`;
+        const newSignalLayerId = createLayerID();
         // Change the signal color
         changeSignalColor(map, currentColor, newSignalLayerId, signalLayerId);
-        timestamp = newTimestamp;
-
+    });
+    const mockData = [
+        { type: "TrafficLight",deviceID:"178be",tlID:"hamburg/328_36",longitude:"9.9507419",latitude:"53.5558168"},
+        { type: "TrafficLightChange", deviceID:"178be",tlID:"hamburg/328_36",state:"red" },
+    ];
+    mockData.forEach(data => {
+        if (data.type === "TrafficLight") {
+            map.on('load', function() {
+                createTrafficLight(map, data.tlID, data.longitude, data.latitude);
+            });
+        } else if (data.type === "TrafficLightChange") {
+            map.on('load', function() {
+                updateTrafficLight(map, data.tlID, data.state);
+            });
+        }
     });
 };
 
--- js/trafficLight.js
+++ js/trafficLight.js
@@ -1,4 +1,6 @@
-function addLayer(map, tb, signalColor, signalLayerId) {
+const signalLayerIdPrefix = 'custom-threebox-signal-';
+var trafficLights = {}; //the dictionary stores the tlID and the corresponding layerId and coords.
+function addLayer(map, tb, signalColor, signalLayerId, coords) {
     const newModelOptions = {
         obj: `../3dModells/trafficlight_${signalColor}.gltf`,
         type: 'gltf',
@@ -19,7 +21,7 @@ function addLayer(map, tb, signalColor, signalLayerId) {
                     { defaultLights: true }
                 );
                 tb.loadObj(newModelOptions, (model) => {
-                    model.setCoords([10.006742457554765, 53.54106874310412]);
+                    model.setCoords(coords);
                     tb.add(model);
                 });
             },
@@ -33,22 +35,56 @@ function addLayer(map, tb, signalColor, signalLayerId) {
 function loadTrafficSignalModel(map, signalColor, signalLayerId) {
     
     map.on('style.load', () => {
-        addLayer(map, tb, signalColor, signalLayerId);
+        const coords = [10.006742457554765, 53.54106874310412];
+        addLayer(map, tb, signalColor, signalLayerId, coords);
     });
 }
 
 function changeSignalColor(map, signalColor, newSignalLayerId, signalLayerId) {
-        addLayer(map, tb, signalColor, newSignalLayerId);
-        setTimeout(() => {
-            if (map.getLayer(signalLayerId)) {
-                map.removeLayer(signalLayerId);
-            }
-        }, 500);
+    const coords = [10.006742457554765, 53.54106874310412];
+    addLayer(map, tb, signalColor, newSignalLayerId, coords);
+    setTimeout(() => {
+        if (map.getLayer(signalLayerId)) {
+            map.removeLayer(signalLayerId);
+        }
+    }, 500);
 }
-
-
-
-
-
-
-
+function createLayerID(){
+    const Timestamp = new Date().getTime();
+    const signalLayerId = `${signalLayerIdPrefix}${Timestamp}`;
+    return signalLayerId;
+}
+//data.type === "TrafficLight"
+function createTrafficLight(map, tlID, longitude, latitude){
+    if (!window.tb) {
+        console.error("tb is not defined.");
+        return;
+    }
+    console.log("Creating traffic light:", tlID, longitude, latitude);
+    var signalLayerId = createLayerID();
+    var coords = [longitude, latitude];
+    trafficLights[tlID] = {
+        signalLayerId: signalLayerId,
+        coords: coords,
+    }
+    console.log("trafficLights[tlID]:" + trafficLights[tlID].signalLayerId + trafficLights[tlID].coords);
+    addLayer(map, tb, 'grey', signalLayerId, coords);
+}
+//data.type === "TrafficLightChange"
+function updateTrafficLight(map, tlID, state){
+    console.log("Updating traffic light:", tlID, state);
+    var oldTrafficLight = trafficLights[tlID];
+    var newSignalLayerId = createLayerID();
+    var coords = oldTrafficLight.coords;
+    addLayer(map, tb, state, newSignalLayerId, coords);
+    setTimeout(() => {
+        if (oldTrafficLight && map.getLayer(oldTrafficLight.signalLayerId)) {
+            map.removeLayer(oldTrafficLight.signalLayerId);
+            delete trafficLights[tlID]; 
+        }
+    }, 500);
+    trafficLights[tlID] = {
+        signalLayerId: newSignalLayerId,
+        coords: coords,
+    }
+}
