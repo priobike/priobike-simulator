87d23f61a4b3fd7e18831bc273c469e4a67380d2
tom-starke <chillkroete99@gmx.de>
2024-01-14 21:10:39 +0100

    replace minimap marker with arrow

--- js/main.js
+++ js/main.js
@@ -60,6 +60,21 @@ window.onload = (event) => {
         changeSignalColor(map, currentColor, newSignalLayerId, signalLayerId);
     });
 
+    // replace minimap marker with arrow
+    const markerWrapper = document.getElementsByClassName('mapboxgl-marker')[0];
+    markerWrapper.style.marginTop = "65px";
+    markerWrapper.innerHTML = `
+    <?xml version="1.0" encoding="iso-8859-1"?>
+        <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+        <svg fill="#000000" height="35px" width="35px" version="1.1" viewBox="0 0 511.995 511.995" xml:space="preserve">
+        <g>
+            <g>
+                <path d="M509.758,480.649L275.091,11.315c-7.232-14.464-30.955-14.464-38.187,0L2.238,480.649
+                    c-4.267,8.576-2.304,18.944,4.8,25.365c7.147,6.464,17.664,7.317,25.749,2.176l223.211-142.037l223.21,142.037
+                    c3.52,2.219,7.488,3.328,11.456,3.328c5.141,0,10.261-1.856,14.293-5.504C512.062,499.593,514.024,489.225,509.758,480.649z"/>
+            </g>
+        </g>
+    </svg>`;
 };
 
 function displayMap(map)
