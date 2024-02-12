421b12d44700c8fbc55ee95a94687b67cb3e7432
PaulPickhardt <s2137275@msx.tu-dresden.de>
2024-02-12 13:09:04 +0100

    Fix simulator id

--- js/mqtt.js
+++ js/mqtt.js
@@ -5,7 +5,7 @@ let connectedDeviceName = '';
 let disconnectTimer;
 let connectionRequestCounter = 0;
 let currentRouteCoordinates = [];
-let simulatorID = Math.random(999);
+let simulatorID = Math.floor(Math.random() * 999)
 
 function connectToMqtt() {
     client = mqtt.connect('ws://priobike.vkw.tu-dresden.de:20037/mqtt', {
@@ -27,6 +27,12 @@ function connectToMqtt() {
         console.log(`Received message on topic ${receivedTopic}: ${message.toString()}`);
         handleMessage(message);
     });
+
+    // Set the simulator id.
+    
+
+
+
 }
 
 function handleMessage(message) {
@@ -106,7 +112,7 @@ function pair(deviceID, deviceName)
     console.log("Connected to " + deviceName + ", deviceID: " + deviceID);
 
     // gib Rückmeldung an App das verbunden wurde
-    client.publish("simulator", '{"type":"PairSimulatorAck", "deviceID":"' + connectedDeviceID + ', "simulatorID": ' + simulatorID + '}');
+    client.publish("simulator", '{"type":"PairSimulatorAck", "deviceID":"' + connectedDeviceID + '", "simulatorID": ' + simulatorID + '}');
 
     removeAllMessages();
     connectionRequestCounter = 0;
